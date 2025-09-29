d2ff8094070b36f5d20e228c5f7b78fdcec39583
Brayan Macedo <macedobarbosab@gmail.com>
2025-09-29 12:53:06 +0200

    página conectada con PokeAPI, falta ordenar los datos

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,39 @@
-const pokemon = "cyndaquil";
 const statsList = document.getElementById("stats");
+const ruido = document.getElementById("ruido");
+const input = document.getElementById("pokemonInput");
+const btn = document.getElementById("buscarBtn");
+const imgFront = document.getElementById("pokeImgFr");
+const imgBack = document.getElementById("pokeImgBack");
 
-fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-.then((response) => response.json())
-.then((data) => {
-    const stats = data.stats;
-    stats.forEach((stat) => {
-        const statLi = document.createElement("li");
-        statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
-        statsList.appendChild(statLi);
-    });
-}).catch((error) => console.error("Error al obtener los datos: ", error))
+const limpiar = () => {
+    statsList.innerHTML = "";
+}
+
+btn.addEventListener("click", () => {
+    const pokemon = input.value.trim().toLowerCase();
+            limpiar();
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const stats = data.stats;
+        const cries = data.cries.latest;
+        const sprite_back = data.sprites.back_default;
+        const sprite_front = data.sprites.front_default;
+   
+
+        stats.forEach((stat) => {
+          const statLi = document.createElement("li");
+          statLi.textContent = `${stat.stat.name}: ${stat.base_stat}`;
+          statsList.appendChild(statLi);
+        });
+
+        ruido.src = cries;
+        pokeImgFr.src = sprite_front;
+        pokeImgBack.src = sprite_back;
+
+        
+      })
+      .catch((error) => console.error("Error al obtener los datos: ", error));
+});
+
